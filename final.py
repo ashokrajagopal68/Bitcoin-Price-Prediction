cad46f88a8296933d80f59e181df5206d3c0abe3
ashok <ashok.ar.jnv14@gmail.com>
2022-09-15 00:01:07 +0530

    deploy on Streamlit

--- final.py
+++ final.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tensorflow import keras
